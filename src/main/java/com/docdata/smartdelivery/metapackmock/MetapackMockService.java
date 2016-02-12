ad766a8e782aea7189f369e4a747cdea700e0b44
622800 <622800@PC0271.nl.docdata.com>
2016-02-12 14:53:50 +0100

    Adds implementation of webmethod printParcel

--- src/main/java/com/docdata/smartdelivery/metapackmock/MetapackMockService.java
+++ src/main/java/com/docdata/smartdelivery/metapackmock/MetapackMockService.java
@@ -1,8 +1,23 @@
 package com.docdata.smartdelivery.metapackmock;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService
 public class MetapackMockService {
+	
+	@WebMethod
+	public ShippingParameter[] printParcel(ShippingParameter[] inputParameters) {
+		List<ShippingParameter> outputParameters = new ArrayList<>();
+		for (ShippingParameter sp : inputParameters) {
+			if (sp.getName().equals("System.RequestId")) {
+				outputParameters.add(sp);
+			}
+		}
+		return outputParameters.toArray(new ShippingParameter[outputParameters.size()]);
+	}
 
 }
